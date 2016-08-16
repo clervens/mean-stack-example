81529ba8286f2f90e402d9895b59616ca6beec72
Clervens Volcy <clervens.volcy@gmail.com>
2016-08-15 20:35:03 -0400

    Install Pug view engine (formerly known as Jade)

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-var http = require('http');
+var http = require('http'),
+	path	 = require('path');
 var express = require('express');
 
 var app = express();
 var server = http.createServer(app);
 
 app.use(express.static(path.resolve(__dirname, 'client')));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+/* GET home page. */
+app.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mean Stack Exemple' });
+});
 
 server.listen(process.env.PORT || 3030, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
